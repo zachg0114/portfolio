9073253b8f7d6f048279009aea174a92b42f3ce3
Zach <113333499+ZachGTheG@users.noreply.github.com>
2024-11-19 17:39:45 -0500

    final ui fix for now?

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/navbar";
+import { Navbar, NavbarContent, NavbarItem } from "@nextui-org/navbar";
 import Link from "next/link";
 
 export default function CustomNavbar() {
@@ -8,37 +8,41 @@ export default function CustomNavbar() {
       maxWidth="full"
       className="bg-[#0a0c1a] text-white w-full overflow-hidden"
     >
-      <NavbarBrand>
+      {/* Conditional rendering for the brand name */}
+      <div className="hidden sm:block">
         <Link href="/">
           <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl whitespace-nowrap">
             Zachary Greene
           </h1>
         </Link>
-      </NavbarBrand>
+      </div>
+
       <NavbarContent
-        justify="end"
-        className="flex items-center gap-4 flex-nowrap overflow-x-auto whitespace-nowrap text-sm sm:text-base"
+        justify="center sm:end"
+        className="flex items-center gap-4 text-sm sm:text-base w-full"
       >
-        <NavbarItem>
-          <Link href="/" className="px-2 sm:px-4">
-            Home
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href="/about" className="px-2 sm:px-4">
-            About
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <a
-            href="https://drive.google.com/file/d/1fxjLNMYXmz_nkYEnqLHqEGXVwtJiV2Mz/view?usp=sharing"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="px-2 sm:px-4"
-          >
-            View Resume
-          </a>
-        </NavbarItem>
+        <div className="flex flex-row sm:justify-end justify-center w-full">
+          <NavbarItem>
+            <Link href="/" className="px-2 sm:px-4">
+              Home
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link href="/about" className="px-2 sm:px-4">
+              About
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <a
+              href="https://drive.google.com/file/d/1fxjLNMYXmz_nkYEnqLHqEGXVwtJiV2Mz/view?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="px-2 sm:px-4"
+            >
+              View Resume
+            </a>
+          </NavbarItem>
+        </div>
       </NavbarContent>
     </Navbar>
   );
