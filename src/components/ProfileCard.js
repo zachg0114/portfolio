92c39d6979e4d813a98d8876465a9439b01f0b75
Zach <113333499+ZachGTheG@users.noreply.github.com>
2024-12-16 23:59:28 -0500

    remove of profile image again

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -3,13 +3,13 @@ import { Image } from "@nextui-org/react";
 export default function ProfileCard({ imageSrc, name }) {
   return (
     <div className="flex flex-col items-center">
-      <div className="w-72 h-72 rounded-full overflow-hidden">
+      {/* <div className="w-72 h-72 rounded-full overflow-hidden">
         <Image
           alt="Profile picture"
           className="object-cover w-full h-full"
           src={imageSrc}
         />
-      </div>
+      </div> */}
       <h2 className="text-4xl font-bold mt-6 text-gradient bg-gradient-to-r from-[#d8b4fe] via-[#a855f7] to-[#7e22ce] bg-clip-text text-transparent">{name}</h2>
     </div>
   );
